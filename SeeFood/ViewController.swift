278997dd2c34759d911b39e879c474545bee779a
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-05-03 18:39:14 +0600

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Arman Myrzakanurov on 03.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
